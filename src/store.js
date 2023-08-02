16e8ce2fc7578e8886931e2354a9a56da5aa2f65
Sachin M K <sachinmk27@gmail.com>
2023-08-02 22:44:03 +0530

    Add Redux devtools

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import rootReducer from './reducers';
 import middlewares from './middlewares';
 
-const store = createStore(rootReducer, middlewares);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(middlewares));
 
 export default store;
